d3e82d6a190b5de885e05f0ac2b243e2202ac0ee
Trevi Awater <awatertrevi@gmail.com>
2015-07-11 13:35:46 +0200

    Removed test file.

--- test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-E:
-CD Github\Skype-Ad-Blocker
-IF /I "%ip%" EQU "" SET ip=127.0.0.1
-FOR /F "tokens=* delims=" %%x IN (domains.txt) DO >nul FIND "%ip% %%x" "C:\Windows\System32\drivers\etc\hosts" && (ECHO '%ip% %%x' already in hosts file, skipping . . .) || (ECHO %ip% %%x >> "C:\Windows\System32\drivers\etc\hosts")
-
-echo.
-pause >nul
-exit
